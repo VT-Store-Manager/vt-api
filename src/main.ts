365d1ce6ab759bcc1a49d0b7d2a929c2f96b6661
quanqvinh <nguyenquanqvinh@gmail.com>
2023-03-01 00:27:58 +0700

    Config data validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@/app/app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+		})
+	)
 	await app.listen(3000)
 }
 bootstrap()
