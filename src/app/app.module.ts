d1d22515feaeb35118bb5d8b9ed02199ed834ff2
dhinsu <62090824+quanqvinh@users.noreply.github.com>
2023-02-28 23:02:04 +0700

    Config NestJS hook processors (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
+import { HttpExceptionFilter } from '@/common/filters/http-exception.filter'
+import { MongoExceptionFilter } from '@/common/filters/mongo-exception.filter'
+import { TransformInterceptor } from '@/common/interceptors/transform.interceptor'
 import configuration from '@/config/configuration'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
 import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose'
 
 import { AppController } from './app.controller'
@@ -24,6 +28,20 @@ import { UserModule } from './user/user.module'
 		}),
 	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [
+		AppService,
+		{
+			provide: APP_FILTER,
+			useClass: HttpExceptionFilter,
+		},
+		{
+			provide: APP_FILTER,
+			useClass: MongoExceptionFilter,
+		},
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: TransformInterceptor,
+		},
+	],
 })
 export class AppModule {}
