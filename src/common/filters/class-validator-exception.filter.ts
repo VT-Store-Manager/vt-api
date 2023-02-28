c1ac9374aa47ceed180a327a8c3ec0e7d3e7a72d
dhinsu <62090824+quanqvinh@users.noreply.github.com>
2023-03-01 00:29:50 +0700

    Config data validator (#3)

---
+++ src/common/filters/class-validator-exception.filter.ts
@@ -0,0 +1,36 @@
+import {
+	ArgumentsHost,
+	Catch,
+	ExceptionFilter,
+	HttpStatus,
+} from '@nestjs/common'
+import { ValidationError } from 'class-validator'
+import { Response } from 'express'
+
+@Catch(ValidationError)
+export class ClassValidatorExceptionFilter implements ExceptionFilter {
+	catch(exception: ValidationError | ValidationError[], host: ArgumentsHost) {
+		const context = host.switchToHttp()
+		const response = context.getResponse<Response>()
+
+		if (!Array.isArray(exception)) {
+			exception = [exception]
+		}
+
+		response.status(HttpStatus.BAD_REQUEST).json({
+			timestamp: new Date().toISOString(),
+			message: `Validation error${
+				exception.length > 1 ? 's' : ''
+			} in your request`,
+			errors: exception.reduce((pre, cur) => {
+				return {
+					...pre,
+					[cur.property]: {
+						value: cur.value,
+						constraints: Object.values(cur.constraints),
+					},
+				}
+			}, {}),
+		})
+	}
+}
