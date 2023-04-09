89e268c29bc89365d9a59303901fc894d219e266
jinsu <nguyenquanqvinh@gmail.com>
2023-04-09 20:17:39 +0700

    Implement API - Get member card

---
+++ src/app/member-rank/member-app/dto/response.dto.ts
@@ -0,0 +1,10 @@
+export class MemberRankCardDTO {
+	id: string
+	name: string
+	scores: number
+	rankName: string
+	nextRankName: string | null
+	background: string
+	nextRank: number | null
+	status: string
+}
