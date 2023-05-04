b22a20134bf1c1c8f69cb2048357bc8e4a9ee940
dhinsu <nguyenquanqvinh@gmail.com>
2023-05-05 00:28:50 +0700

    SM-47 Implement API - Get used voucher (#34)

--- src/app/member-voucher/admin-app/dto/response.dto.ts
+++ src/app/member-voucher/admin-app/dto/response.dto.ts
@@ -3,15 +3,3 @@ export class CreateMemberVoucherDTO {
 	successCount: number
 	failedList: string[]
 }
-
-export class AvailableMemberVoucherDTO {
-	id: string
-	code: string
-	name: string
-	image: string
-	partner?: string
-	sliderImage: string
-	from: number
-	to: number
-	description: string
-}
--- src/app/member-voucher/member-voucher.module.ts
+++ src/app/member-voucher/member-voucher.module.ts
@@ -1,5 +1,9 @@
 import { MemberRank, MemberRankSchema } from '@/schemas/member-rank.schema'
 import {
+	MemberVoucherHistory,
+	MemberVoucherHistorySchema,
+} from '@/schemas/member-voucher-history.schema'
+import {
 	MemberVoucher,
 	MemberVoucherSchema,
 } from '@/schemas/member-voucher.schema'
@@ -7,6 +11,7 @@ import { Voucher, VoucherSchema } from '@/schemas/voucher.schema'
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
+import { SettingModule } from '../setting/setting.module'
 import { MemberVoucherAdminController } from './admin-app/member-voucher_admin.controller'
 import { MemberVoucherAdminService } from './admin-app/member-voucher_admin.service'
 import { MemberVoucherMemberController } from './member-app/member-voucher_member.controller'
@@ -18,7 +23,9 @@ import { MemberVoucherMemberService } from './member-app/member-voucher_member.s
 			{ name: Voucher.name, schema: VoucherSchema },
 			{ name: MemberVoucher.name, schema: MemberVoucherSchema },
 			{ name: MemberRank.name, schema: MemberRankSchema },
+			{ name: MemberVoucherHistory.name, schema: MemberVoucherHistorySchema },
 		]),
+		SettingModule,
 	],
 	controllers: [MemberVoucherAdminController, MemberVoucherMemberController],
 	providers: [MemberVoucherAdminService, MemberVoucherMemberService],
