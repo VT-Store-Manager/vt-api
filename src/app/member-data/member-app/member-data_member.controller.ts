58f9a6c82db35cefd59636de1190ab503256fed9
dhinsu <nguyenquanqvinh@gmail.com>
2023-05-06 03:57:30 +0700

    SM-49 Implement API get point history (#36)

---
+++ src/app/member-data/member-app/member-data_member.controller.ts
@@ -0,0 +1,32 @@
+import { CurrentUser } from '@/app/auth/decorators/current-user.decorator'
+import { JwtAccess } from '@/app/auth/decorators/jwt.decorator'
+import { Role } from '@/common/constants'
+import { ApiSuccessResponse } from '@/common/decorators/api-success-response.decorator'
+import { UserPayload } from '@/types/token.dto'
+import { Controller, Get, Query } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+
+import { GetPointHistoryDTO } from './dto/get-point-history.dto'
+import { PointHistoryResultDTO } from './dto/response.dto'
+import { MemberDataMemberService } from './member-data_member.service'
+
+@Controller({
+	path: 'member-data',
+	version: '1',
+})
+@ApiTags('member-app > member-data')
+export class MemberDataMemberController {
+	constructor(
+		private readonly memberDataMemberService: MemberDataMemberService
+	) {}
+
+	@Get('point-history')
+	@JwtAccess(Role.MEMBER)
+	@ApiSuccessResponse(PointHistoryResultDTO)
+	async getPointHistory(
+		@CurrentUser() { sub: userId }: UserPayload,
+		@Query() query: GetPointHistoryDTO
+	) {
+		return await this.memberDataMemberService.getPointHistory(userId, query)
+	}
+}
