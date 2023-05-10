3fbef532510d706ab8fd42e704ff35203045afab
jinsu <nguyenquanqvinh@gmail.com>
2023-05-10 23:01:24 +0700

    SM-62 Implement api - Get all notifications

--- src/app/modules/notification/member-app/notification_member.controller.ts
+++ src/app/modules/notification/member-app/notification_member.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common'
+import { Role } from '@/common/constants'
+import { CurrentUser } from '@module/auth/decorators/current-user.decorator'
+import { JwtAccess } from '@module/auth/decorators/jwt.decorator'
+import { Controller, Get } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 
 import { NotificationMemberService } from './notification_member.service'
+import { ApiSuccessResponse } from '@/common/decorators/api-success-response.decorator'
+import { MemberNotificationItemDTO } from './dto/response.dto'
 
 @Controller({
 	path: 'member/notification',
@@ -12,4 +17,13 @@ export class NotificationMemberController {
 	constructor(
 		private readonly notificationService: NotificationMemberService
 	) {}
+
+	@Get('all')
+	@JwtAccess(Role.MEMBER)
+	@ApiSuccessResponse(MemberNotificationItemDTO, 200, true)
+	async getAllNotifications(
+		@CurrentUser('sub') memberId: string
+	): Promise<MemberNotificationItemDTO[]> {
+		return await this.notificationService.getAll(memberId)
+	}
 }
--- src/app/modules/notification/member-app/notification_member.service.ts
+++ src/app/modules/notification/member-app/notification_member.service.ts
@@ -1,9 +1,11 @@
-import { Model } from 'mongoose'
-import { Injectable } from '@nestjs/common'
+import { Model, Types } from 'mongoose'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 
 import { NotificationDocument, Notification } from '@schema/notification.schema'
 import { MemberData, MemberDataDocument } from '@schema/member-data.schema'
+import { MemberNotificationItemDTO } from './dto/response.dto'
+import { SettingMemberAppService } from '../../setting/services/setting-member-app.service'
 
 @Injectable()
 export class NotificationMemberService {
@@ -11,6 +13,53 @@ export class NotificationMemberService {
 		@InjectModel(Notification.name)
 		private readonly notificationModel: Model<NotificationDocument>,
 		@InjectModel(MemberData.name)
-		private readonly memberDataModel: Model<MemberDataDocument>
+		private readonly memberDataModel: Model<MemberDataDocument>,
+		private readonly settingMemberAppService: SettingMemberAppService
 	) {}
+
+	async getAll(memberId: string) {
+		const [memberData, { notification: notificationSetting }] =
+			await Promise.all([
+				this.memberDataModel
+					.aggregate<{ notifications: MemberNotificationItemDTO[] }>([
+						{
+							$match: {
+								member: new Types.ObjectId(memberId),
+							},
+						},
+						{
+							$project: {
+								notifications: {
+									$map: {
+										input: '$notifications',
+										as: 'e',
+										in: {
+											id: '$$e._id',
+											name: '$$e.name',
+											description: '$$e.name',
+											time: { $toLong: '$$e.createdAt' },
+											image: '$$e.image',
+											targetId: '$$e.targetId',
+											checked: '$$e.checked',
+											type: '$$e.type',
+										},
+									},
+								},
+								_id: false,
+							},
+						},
+					])
+					.exec(),
+				this.settingMemberAppService.getData({ notification: true }),
+			])
+		if (memberData.length === 0) {
+			throw new BadRequestException('Member data not found')
+		}
+		return memberData[0].notifications
+			.slice(0, notificationSetting.limit)
+			.map(item => ({
+				...item,
+				...(!item.image ? { image: notificationSetting.defaultImage } : {}),
+			}))
+	}
 }
